4dd9b5b76d1723e9bad1d45c9b841d05af563c27
maverickjin88 <maverickjin88@gmail.com>
2019-12-14 23:37:48 -0700

    5 divide and conquer

---
+++ easy/5_Longest_Common_Prefix/binary_search.py
@@ -0,0 +1,46 @@
+class Solution:
+
+    def isCommonPrefix(self, strs: List[str], length: int) -> bool:
+
+        str1 = strs[0][0:length]
+        # print(str1, len)
+
+        for i in range(1, len(strs)):
+            if strs[i][0:length] != str1:
+                return False
+        return True
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        if strs is None or len(strs) == 0:
+            return ""
+
+        minLen = float("inf")
+
+        for str in strs:
+            if len(str) < minLen:
+                minLen = len(str)
+
+        low = 1
+        high = int(minLen)
+
+        # print('before', low, high)
+
+        while low <= high:
+
+            middle = int((low + high) / 2)
+            if self.isCommonPrefix(strs, middle):
+                low = middle + 1
+            else:
+                high = middle - 1
+
+            # print('loop', low, high, middle)
+
+        middle = int((low + high) / 2)
+
+        # print('result', low, high, strs[0][0:middle])
+        return strs[0][0:middle]
+
+
+
+
