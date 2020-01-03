349bbeacb9382bcecc1d3c31b31c9484c3304b93
esther-yyt <yyao63@asu.edu>
2020-01-02 20:40:43 -0700

    update longest palindromic substring using manacher's algorithm

---
+++ medium/Longest_Palindromic_Substring/manacher's_algorithm_yiting.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        new = "#".join("${}&".format(s))
+        n = len(new)
+        P = [0 for i in range(n)]
+        C = 0
+        R = 0
+        for i in range(1, n - 1):
+            j = C - (i - C)
+            if R - i > P[j]:
+                P[i] = P[j]
+            else:
+                P[i] = R - i
+
+            while new[i + 1 + P[i]] == new[i - 1 - P[i]]:
+                P[i] += 1
+
+            if P[i] + i > R:
+                R = P[i] + i
+                C = i
+
+        l, i = max((l, i) for i, l in enumerate(P))
+        return s[(i - l) / 2:(i + l) / 2]
