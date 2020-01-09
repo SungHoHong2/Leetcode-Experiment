377d8278a577e23413a36849fe36584923e7456b
maverickjin88 <maverickjin88@gmail.com>
2020-01-09 16:52:43 -0700

    easyt

--- medium/Longest_Substring_Without_Repeating_Characters/sungho/sliding_window_opt.py
+++ medium/Longest_Substring_Without_Repeating_Characters/sungho/sliding_window_opt.py
@@ -2,25 +2,30 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
         n = len(s)
-
-        # key = char  value = index of the char
         chkTable = {}
+        ans = i = j = 0
 
-        ans = 0
-        i = 0
+        # i
+        #    j
+        # abcabcbb
+        # 1234
+
+        # (skip)
+        #    i
+        #    j
+        # abcabcbb
+        # 1234
 
         for j in range(0, n):
 
+            # let the (i index) skip all the way to the (j index)
             if s[j] in chkTable:
                 i = max(chkTable[s[j]], i)
 
             ans = max(ans, j - i + 1)
             chkTable[s[j]] = j + 1
 
+            print(ans, j, i)
+
         return ans
-
-
-
-
-
 
