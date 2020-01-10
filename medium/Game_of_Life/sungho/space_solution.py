ba26b89fcc691132e9b8b537e779dee099c0f86d
maverickjin88 <maverickjin88@gmail.com>
2020-01-10 13:30:33 -0700

    updated Game of Life

---
+++ medium/Game_of_Life/sungho/space_solution.py
@@ -0,0 +1,44 @@
+# 1. Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+# 2. Any live cell with two or three live neighbors lives on to the next generation.
+# 3. Any live cell with more than three live neighbors dies, as if by over-population..
+# 4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+# perform simultaneous updation as is required in the question
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        # Neighbors array to find 8 neighboring cells for a given cell
+        neighbors = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
+
+        rows = len(board)
+        cols = len(board[0])
+
+        # Create a copy of the original board
+        copy_board = [[board[row][col] for col in range(cols)] for row in range(rows)]
+
+        # Iterate through board cell by cell.
+        for row in range(rows):
+            for col in range(cols):
+
+                # For each cell count the number of live neighbors.
+                live_neighbors = 0
+                for neighbor in neighbors:
+
+                    r = (row + neighbor[0])
+                    c = (col + neighbor[1])
+
+                    # Check the validity of the neighboring cell and if it was originally a live cell.
+                    # The evaluation is done against the copy, since that is never updated.
+                    if (r < rows and r >= 0) and (c < cols and c >= 0) and (copy_board[r][c] == 1):
+                        live_neighbors += 1
+
+                # Rule 1 or Rule 3
+                if copy_board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    board[row][col] = 0
+                # Rule 4
+                if copy_board[row][col] == 0 and live_neighbors == 3:
+                    board[row][col] = 1
+
