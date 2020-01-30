a0845a24d1461db00c7eed9458e96b7ca5bcf21c
maverickjin88 <maverickjin88@gmail.com>
2020-01-29 20:48:08 -0700

    Insert Delete Get Random

--- medium/Insert_Delete_GetRandom/sungho/hashmap_array.py
+++ medium/Insert_Delete_GetRandom/sungho/hashmap_array.py
@@ -1,19 +1,18 @@
 class RandomizedSet:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.rdict = {}
         self.rlist = []
 
     def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
+        # if there is nothing in the dictionary return false
         if val in self.rdict:
             return False
+
+        # add the element to the hashmap as (value, index)
         self.rdict[val] = len(self.rlist)
+
+        # append to the array
         self.rlist.append(val)
         return True
 
@@ -21,38 +20,38 @@ class RandomizedSet:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
+
+        # if the value is in the hashmap
         if val in self.rdict:
-            # move the last element to the place idx of the element to delete
-            # remove c
-            # 1 2 3 4 5
-            # a b c d e
+            # purpose is to override the target element with the last element
 
+            # get the value of last element from the array
             last_element = self.rlist[-1]
-            # 1 2 3 4 [5]
-            # a b c d  e
+            # 1 2 3 4  5
+            # a b c d [e]
 
+            # get the index of the target element
             idx = self.rdict[val]
             # 1 2  3  4 5
             # a b [c] d e
 
-            # override the last element to the current position of the array
+            # override the target element with the last element in the array
             self.rlist[idx] = last_element
             # 1 2  3  4 5
             # a b [e] d e
 
-            # update the dictionary of the last element with the current index
+            # override the target element with the last element in the hashmap
             self.rdict[last_element] = idx
 
-            # delete the last element
+            # delete the last element in the array
             self.rlist.pop()
+            # delete the element in the hashmap
             del self.rdict[val]
             return True
 
         return False
 
     def getRandom(self) -> int:
-        """
-        Get a random element from the set.
-        """
+        # return random choice
         return random.choice(self.rlist)
 
