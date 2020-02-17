33e95b2c77e095b97af14e57f6f98b0814bca437
maverickjin88 <maverickjin88@gmail.com>
2020-02-17 12:10:05 -0700

    update

--- medium/Letter_Combinations_of_a_Phone_Number/sungho/iteration.py
+++ medium/Letter_Combinations_of_a_Phone_Number/sungho/iteration.py
@@ -12,16 +12,27 @@ class Solution:
             '9': 'wxyz',
         }
 
-        all_combinations = [''] if digits else []
+        if not digits:
+            return []
 
+        # init the array
+        all_combinations = ['']
+
+        # iterate the digits
         for digit in digits:
 
+            # create temp list
             current_combinations = list()
+
+            # iterate the alphabets of the digit
             for letter in interpret_digit[digit]:
 
+                # iterate all the possible combination
                 for combination in all_combinations:
+                    # append the letter
                     current_combinations.append(combination + letter)
 
+            # update all the possible combination
             all_combinations = current_combinations
 
         return all_combinations
