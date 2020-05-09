594937c2356a909f09df90151412e086faf29c56
maverickjin88 <maverickjin88@gmail.com>
2020-05-09 13:48:18 -0700

    update algorithm

---
+++ medium/Minimum_Time_Difference/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        t = sorted(int(t[:2]) * 60 + int(t[-2:]) for t in timePoints)
+
+        t.append(t[0] + 1440)
+
+        return min(b - a for a, b in zip(t[:-1], t[1:]))
