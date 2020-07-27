48f3afc16e84a663e36227e2870a3ee36394f828
maverickjin88 <maverickjin88@gmail.com>
2020-07-26 20:14:19 -0700

    update Maximum Subarray

--- easy/Maximum_subarray/source/dynamic.py
+++ easy/Maximum_subarray/source/dynamic.py
@@ -1,12 +1,12 @@
 class Solution(object):
     def maxSubArray(self, nums):
         # set the current sum and the maxsum to the first element
-        curr_sum = max_sum = nums[0]
+        currSum = maxSum = nums[0]
         # iterate through the numbers
         for i in range(1, len(nums)):
             # record the current sum of the subarray
-            curr_sum = max(nums[i], curr_sum + nums[i])
+            currSum = max(nums[i], currSum + nums[i])
             # record the maximum sum of the subarray
-            max_sum = max(max_sum, curr_sum)
+            maxSum = max(currSum, maxSum)
         # return the maximum sum
-        return max_sum
+        return maxSum
