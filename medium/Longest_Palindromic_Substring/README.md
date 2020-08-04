41a7d6db146392c62514fa436b7cd73bbd812900
maverickjin88 <maverickjin88@gmail.com>
2020-08-04 12:36:50 -0700

    revise longest palindrome

--- medium/Longest_Palindromic_Substring/README.md
+++ medium/Longest_Palindromic_Substring/README.md
@@ -1,12 +1,43 @@
 ### Longest Palindromic Substring
 
-- **Brute Force**
-    - [source code](source/brute.py)
+**Brute Force**
+- [source code](source/brute.py)
+```python
+class Solution:
+    # function for checking the largest palindrome
+        # set the left and right index for the odd palindrome
+        # set the variable for the largest odd palindrome
+        # loop until left and right reach the end 
+            # if it is a plaindrome 
+                # increase the size of the odd paliindrome
+            # if it is not a palindrome 
+                # escape the loop
+            # increase the left and right index 
+        # left and right index for the even palindrome
+        # find the even palindrome using the same method of finding the odd palindrome 
+        # if odd palindrome is larger 
+            # return the index for the odd palindrome 
+        # if even palindrome is larger 
+            # return the index for the even palindrome and note that even has no centerpoint 
 
-- **Dynamic Programming**
-    - [concepts](images/dynamic.png)
-    - [source code](source/dynamic.py)
+    def longestPalindrome(self, s: str) -> str:
+        # set the variable for the largest substring 
+        # if the string is none
+            # return empty string 
+        # if the string length is only one 
+            # return the string
+        # iterate the string
+            # invoke palindrome checker and get the left and right index 
+            # if the length of the substring creates a new record
+                # update the largest substring
+        # return the largest substring
+```
 
-- **Expand Around Center**
-    - [concepts](images/expand.png)
-    - [source code](source/expand.py)
+
+**Dynamic Programming**
+- [concepts](images/dynamic.png)
+- [source code](source/dynamic.py)
+
+**Expand Around Center**
+- [concepts](images/expand.png)
+- [source code](source/expand.py)
--- medium/Longest_Palindromic_Substring/source/brute.py
+++ medium/Longest_Palindromic_Substring/source/brute.py
@@ -1,59 +1,62 @@
 class Solution:
+    # function for checking the largest palindrome
+    def checkMatch(self, s, index):
+        # set the left and right index for the odd palindrome
+        left = index - 1
+        right = index + 1
+        # set the variable for the largest odd palindrome
+        oddLength = 0
+        # loop until left and right reach the end
+        while left >= 0 and right < len(s):
+            # if it is a plaindrome
+            if s[left] == s[right]:
+                # increase the size of the odd paliindrome
+                oddLength += 1
+            # if it is not a palindrome
+            else:
+                # escape the loop
+                break
+            # increase the left and right index
+            left -= 1
+            right += 1
+        # left and right index for the even palindrome
+        left = index - 1
+        right = index
+        # find the even palindrome using the same method of finding the odd palindrome
+        evenLength = 0
+        while left >= 0 and right < len(s):
+            if s[left] == s[right]:
+                evenLength += 1
+            else:
+                break
+            left -= 1
+            right += 1
+        # if odd palindrome is larger
+        if oddLength >= evenLength:
+            # return the index for the odd palindrome
+            return index - oddLength, index + oddLength
+        elif oddLength < evenLength:
+            # return the index for the even palindrome and note that even has no centerpoint
+            return index - evenLength, index + evenLength - 1
 
     def longestPalindrome(self, s: str) -> str:
-
-        checkLongest = 0
-        biggestString = None
-
+        # set the variable for the largest substring
+        maxString = ""
+        # if the string is none
         if len(s) == 0:
+            # return empty string
             return ""
-
+        # if the string length is only one
         if len(s) == 1:
+            # return the string
             return s
-
-        def checkMatch(index):
-
-            oddLength = 0
-            evenLength = 0
-
-            # check the odd palindrome
-            left = index - 1
-            right = index + 1
-
-            while left >= 0 and right < len(s):
-
-                if s[left] == s[right]:
-                    oddLength += 1
-                else:
-                    break
-
-                left -= 1
-                right += 1
-
-            # check the even palindrome
-            left = index - 1
-            right = index
-
-            while left >= 0 and right < len(s):
-
-                if s[left] == s[right]:
-                    evenLength += 1
-                else:
-                    break
-
-                left -= 1
-                right += 1
-
-            if oddLength >= evenLength:
-                return index - oddLength, index + oddLength
-            elif oddLength < evenLength:
-                return index - evenLength, index + evenLength - 1
-
+        # iterate the string
         for i in range(1, len(s)):
-            left, right = checkMatch(i)
-
-            if checkLongest < len(s[left:right + 1]):
-                checkLongest = len(s[left:right + 1])
-                biggestString = s[left:right + 1]
-
-        return biggestString
+            # invoke palindrome checker and get the left and right index
+            left, right = self.checkMatch(s, i)
+            # if the length of the substring creates a new record
+            if len(maxString) < len(s[left:right + 1]):
+                # update the largest substring
+                maxString = s[left:right + 1]
+        # return the largest substring
+        return maxString
