bdb8ecd2f1037cf6f491e53428b9bba095990b29
maverickjin88 <maverickjin88@gmail.com>
2020-08-04 17:57:34 -0700

    revised longest palindrome substring

--- medium/Longest_Palindromic_Substring/source/brute.py
+++ medium/Longest_Palindromic_Substring/source/brute.py
@@ -1,15 +1,15 @@
 class Solution:
     # function for checking the largest palindrome
-    def checkMatch(self, s, index):
+    def chkPalindrome(self, arr, index):
         # set the left and right index for the odd palindrome
         left = index - 1
         right = index + 1
         # set the variable for the largest odd palindrome
         oddLength = 0
         # loop until left and right reach the end
-        while left >= 0 and right < len(s):
+        while left >=0 and right < len(arr):
             # if it is a plaindrome
-            if s[left] == s[right]:
+            if arr[left] == arr[right]:
                 # increase the size of the odd paliindrome
                 oddLength += 1
             # if it is not a palindrome
@@ -24,18 +24,19 @@ class Solution:
         right = index
         # find the even palindrome using the same method of finding the odd palindrome
         evenLength = 0
-        while left >= 0 and right < len(s):
-            if s[left] == s[right]:
+        # if odd palindrome is larger
+        while left >= 0 and right < len(arr):
+            # return the index for the odd palindrome
+            if arr[left] == arr[right]:
                 evenLength += 1
             else:
                 break
             left -= 1
             right += 1
-        # if odd palindrome is larger
+        # if even palindrome is larger
         if oddLength >= evenLength:
-            # return the index for the odd palindrome
-            return index - oddLength, index + oddLength
-        elif oddLength < evenLength:
+            return index - oddLength, index+ oddLength
+        else:
             # return the index for the even palindrome and note that even has no centerpoint
             return index - evenLength, index + evenLength - 1
 
@@ -43,20 +44,16 @@ class Solution:
         # set the variable for the largest substring
         maxString = ""
         # if the string is none
-        if len(s) == 0:
+        if len(s) == 0 or len(s) == 1:
             # return empty string
-            return ""
-        # if the string length is only one
-        if len(s) == 1:
-            # return the string
             return s
         # iterate the string
         for i in range(1, len(s)):
             # invoke palindrome checker and get the left and right index
-            left, right = self.checkMatch(s, i)
+            left, right = self.chkPalindrome(s,i)
             # if the length of the substring creates a new record
-            if len(maxString) < len(s[left:right + 1]):
+            if len(maxString) < len(s[left:right+1]):
                 # update the largest substring
-                maxString = s[left:right + 1]
+                maxString = s[left:right+1]
         # return the largest substring
         return maxString
