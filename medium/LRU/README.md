1aa12edd4de470ca3fda8594d9edda86412e06e1
maverickjin88 <maverickjin88@gmail.com>
2020-08-09 09:36:51 -0700

    update LRU cache

--- medium/LRU/README.md
+++ medium/LRU/README.md
@@ -1,7 +1,27 @@
-### LRU
+### LRU Cache
 
 **Ordered dictionary**
 - [source code](source/OrderedDict.py)
+
+```python
+from collections import OrderedDict
+
+class LRUCache():
+    def __init__(self, capacity: int):
+        # create collections.OrderedDict which is a combination of hashmap and linked-list
+        # indicator of the total size of the LRU
+    def get(self, key):
+        # if there is no key
+            # return -1
+        # set the item to most recently checked
+        # return the result from the hashmap
+    def put(self, key, value):
+        # if the key exists
+            # set the item to most recently checked
+        # update the. value
+        # if the total capacity of the LRU is full remove the least recently checked
+```
+
 
 **HashMap + LinkedList**
 - [source code](source/)
--- medium/LRU/source/OrderedDict.py
+++ medium/LRU/source/OrderedDict.py
@@ -2,33 +2,28 @@ from collections import OrderedDict
 
 class LRUCache():
     def __init__(self, capacity: int):
-        # create OrderedDict which is a combination of hashap and linked-list
+        # create collections.OrderedDict which is a combination of hashmap and linked-list
         self.cache = OrderedDict()
-        # pointer to the total size of the LRU
+        # indicator of the total size of the LRU
         self.capacity = capacity
 
     def get(self, key):
-
         # if there is no key
         if key not in self.cache:
+            # return -1
             return -1
-
         # set the item to most recently checked
         self.cache.move_to_end(key)
-
         # return the result from the hashmap
         return self.cache[key]
 
     def put(self, key, value):
-
         # if the key exists
         if key in self.cache:
             # set the item to most recently checked
             self.cache.move_to_end(key)
-
         # update the. value
         self.cache[key] = value
-
         # if the total capacity of the LRU is full remove the least recently checked
         if len(self.cache) > self.capacity:
             self.cache.popitem(last=False)
