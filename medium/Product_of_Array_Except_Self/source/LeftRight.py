d7bb7b8400e92fcff8528ac00f729fe442e28876
maverickjin88 <maverickjin88@gmail.com>
2020-08-18 09:40:21 -0700

    revising Product of Array Except self

--- medium/Product_of_Array_Except_Self/source/LeftRight.py
+++ medium/Product_of_Array_Except_Self/source/LeftRight.py
@@ -1,27 +1,22 @@
-### Problem Title
-**Left and Right product lists**
-- [Concepts](images/LeftRight.png)
-- [Source code](source/LeftRight.py)
-
-```python
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         # Initiate the left, right and return array
+        left = [0 for i in nums]
+        right = [0 for i in nums]
+        answer = [0 for i in nums]
         # Aggregate the products from the left
+        left[0] = 1
+        for i in range(1, len(nums)):
             # track the history of the aggregated product until one element is left in the nums array
+            left[i] = nums[i - 1] * left[i - 1]
         # Aggregate the products from the left
+        right[len(nums) - 1] = 1
+        for i in reversed(range(len(nums) - 1)):
             # track the history of the aggregated product until one element is left in the nums array
+            right[i] = nums[i + 1] * right[i + 1]
         # Calculate the product that excludes the original element in the nums array
+        for i in range(len(nums)):
             # calculate the products of the left and the right calculation
+            answer[i] = left[i] * right[i]
         # return the answer
-```
-
-**Solution2**
-- [Concepts](images/)
-- [Source code](source/)
-- [Reference #1]() 
-
-**Solution3**
-- [Concepts](images/)
-- [Source code](source/)
-- [Reference #1]()    
+        return answer
