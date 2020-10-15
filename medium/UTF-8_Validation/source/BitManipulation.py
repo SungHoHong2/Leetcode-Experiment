14a1caa7150a2f2da3d55891ceb0f73f33fd4f79
maverickjin88 <maverickjin88@gmail.com>
2020-10-15 11:26:00 -0700

    updated UTF-8 validation

--- medium/UTF-8_Validation/source/BitManipulation.py
+++ medium/UTF-8_Validation/source/BitManipulation.py
@@ -1,41 +1,35 @@
-### UTF-8 Validation
-**String Manipulation**
-- [Source code](source/StringManipulate.py)
-```python
 class Solution:
     def validUtf8(self, data):
         # set the variable to count the total number of grouped bytes
-        # iterate the numbers from the input
-            # convert the numbers into binary representation of 8 bits
-            # if it is the first byte of the group
-                # count the total number of following bytes until zero
-                # continue the loop if the there is only one byte in the group
-                # return false if there is only 1 or more than 4 at the first byte
-            # if the group is more than 1 and the first byte is valid
-                # return false if the most significants 2bits are not 1,0
-            # decrement the total number of bytes of the group
-        # return true if all the bytes in the group are processed
-        pass
-```
-
-**Bit Manipulation**
-- [Source code](source/BitManipulation.py)
-```python
-class Solution:
-    def validUtf8(self, data):
-        # set the variable to count the total number of grouped bytes
+        n_bytes = 0
         # set the mask to check the most significant bit
+        mask1 = 1 << 7
         # set the mask to check the second most significant bit
+        mask2 = 1 << 6
         # iterate the numbers from the input
+        for num in data:
             # set a mask to check the most significant bit
+            mask = 1 << 7
             # if the number is the first byte of the group
+            if n_bytes == 0:
                 # loop until the num is not zero
+                while mask & num:
                     # increase the number of total following bytes
+                    n_bytes += 1
                     # shift the mask to mask the second significant bit
+                    mask = mask >> 1
                 # continue the loop if the there is only one byte in the group
+                if n_bytes == 0:
+                    continue
                 # return false if there is only 1 or more than 4 at the first byte
+                if n_bytes == 1 or n_bytes > 4:
+                    return False
             # if the group is more than 1 and the first byte is valid
+            else:
                 # return false if the most significants 2bits are not 1,0
+                if not (num & mask1 and not (num & mask2)):
+                    return False
             # decrement the total number of bytes of the group
+            n_bytes -= 1
         # return true if all the bytes in the group are processed
-```
+        return n_bytes == 0
