475a23e07def161bb8fef16519bfeba153ff6488
maverickjin88 <maverickjin88@gmail.com>
2020-11-05 15:32:18 -0700

    update

--- medium/Minimum_Domino_Rotations_For_Equal_Row/source/Greedy.py
+++ medium/Minimum_Domino_Rotations_For_Equal_Row/source/Greedy.py
@@ -1,9 +1,9 @@
 class Solution:
-    def check(self, x, n):
+    def check(self, A, B, x):
         # set up the variable for rotations made by A and. B
         rot_a = rot_b = 0
         # iterate the dominos
-        for i in range(0, n):
+        for i in range(0, len(A)):
             # if both A and B are different from the target
             if A[i] != x and B[i] != x:
                 # return false
@@ -18,14 +18,15 @@ class Solution:
                 rot_b += 1
         # return the minimum number of rotations
         return min(rot_a, rot_b)
+
     def minDominoRotations(self, A: List[int], B: List[int]) -> int:
         # invoke the check function and get the minimum number of rotations
-        rotation = self.check(A[0], len(A))
+        rotation = self.check(A, B, A[0])
+
         # if the rotation value is valid or A and B have the same value
         if rotation != -1 or A[0] == B[0]:
-            # return the minimun numvber of rotations
             return rotation
         # if the rotation does not provide any value
         else:
-            # get the return value from B
-            return self.check(B[0], len(B))
+            return self.check(A, B, B[0])
+
