c9a4db3f78ac1e800657486504fcad5ac37f276c
maverickjin88 <maverickjin88@gmail.com>
2020-11-17 11:06:38 -0700

    update minimum window substring

--- hard/Minimum_Window_Substring/README.md
+++ hard/Minimum_Window_Substring/README.md
@@ -26,12 +26,35 @@ class Solution:
         pass
 ```
 
-**Solution2**
+**Optimized Sliding Window**
 - [Concepts](images/)
-- [Source code](source/)
-- [Reference #1]()
-
-**Solution3**
-- [Concepts](images/)
-- [Source code](source/)
-- [Reference #1]()
+    - Consider a scenario:
+        - length of string `t` is way too small than the length of string `s` 
+        - string `s` consists of numerous characters which are not present in `t`
+    - Store the index of the characters in `s` that matches with `t`
+- [Source code](source/SlidingWindowOpt.py)
+```python
+class Solution:
+    def minWindow(self, s, t):
+        # return empty string if there is a invalid input
+        # declare a hashmap that counts the frequency of the unqiue characters of string "t"
+        # get the total number of unique characters of string "t"
+        # filter all the characters from "s" into a new list along with their index.
+        # left and right pointer
+        # keep track of number of characters of string "t" that are matched with string "s"
+        # declare a hashmap that counts the frequency of the unqiue characters of the current window
+        # declare a tuple of the form (window length, left, right)
+        # explore for the characters only in the filtered list instead of entire "s"
+            # get one character from the right to the window
+            # count the frequency of the characters in the window
+            # check if the frequency of the character in the window matches with the string "t"
+            # start recording the answer once the window contains the substring of "t"
+                # record the smallest window
+                # deduct the characters from the left index
+                # once the window does not satisfy the complete subset of string "t"
+                    # reduce the number of matched frequency
+                # shrink the window size
+            # increase the window size
+        # return empty string if there is no match or return the matched substring
+        pass
+```
--- hard/Minimum_Window_Substring/source/SlidingWindowOpt.py
+++ hard/Minimum_Window_Substring/source/SlidingWindowOpt.py
@@ -1,6 +1,7 @@
 class Solution:
     def minWindow(self, s, t):
-        # return empty string if the input is invalid
+
+        # return empty string if there is a invalid input
         if not t or not s:
             return ""
         # declare a hashmap that counts the frequency of the unqiue characters of string "t"
@@ -9,33 +10,40 @@ class Solution:
             dict_t[c] += 1
         # get the total number of unique characters of string "t"
         required = len(dict_t)
+        # filter all the characters from "s" into a new list along with their index.
+        filtered_s = []
+        for i, char in enumerate(s):
+            if char in dict_t:
+                filtered_s.append((i, char))
         # left and right pointer
         l, r = 0, 0
         # keep track of number of characters of string "t" that are matched with string "s"
         formed = 0
-        # declare a hashmap that counts the frequency of the unique characters of the current window
+        # declare a hashmap that counts the frequency of the unqiue characters of the current window
         window_counts = collections.defaultdict(int)
         # declare a tuple of the form (window length, left, right)
-        ans = (float("inf"), None, None)
-        # start expanding the window from the right
-        while r < len(s):
+        ans = float("inf"), None, None
+        # explore for the characters only in the filtered list instead of entire "s"
+        while r < len(filtered_s):
             # get one character from the right to the window
-            character = s[r]
-            # add to the map
+            character = filtered_s[r][1]
+            # count the frequency of the characters in the window
             window_counts[character] += 1
             # check if the frequency of the character in the window matches with the string "t"
-            if character in dict_t and window_counts[character] == dict_t[character]:
+            if window_counts[character] == dict_t[character]:
                 formed += 1
             # start recording the answer once the window contains the substring of "t"
             while l <= r and formed == required:
-                character = s[l]
+                character = filtered_s[l][1]
                 # record the smallest window
-                if r - l + 1 < ans[0]:
-                    ans = (r - l + 1, l, r)
+                end = filtered_s[r][0]
+                start = filtered_s[l][0]
+                if end - start + 1 < ans[0]:
+                    ans = (end - start + 1, start, end)
                 # deduct the characters from the left index
                 window_counts[character] -= 1
                 # once the window does not satisfy the complete subset of string "t"
-                if character in dict_t and window_counts[character] < dict_t[character]:
+                if window_counts[character] < dict_t[character]:
                     # reduce the number of matched frequency
                     formed -= 1
                 # shrink the window size
