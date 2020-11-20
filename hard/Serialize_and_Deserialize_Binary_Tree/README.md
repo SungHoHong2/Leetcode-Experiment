bc7f0ed08459047b4df75350630cefaa0967dc9c
maverickjin88 <maverickjin88@gmail.com>
2020-11-19 20:37:40 -0700

    update Serialized and Deserialize binary tree

--- hard/Serialize_and_Deserialize_Binary_Tree/README.md
+++ hard/Serialize_and_Deserialize_Binary_Tree/README.md
@@ -42,5 +42,40 @@ class Codec:
 **BFS**
 - [Source code](source/BFS.py)
 ```python
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Codec:
+
+    def serialize(self, root):
+        # return empty string if the input is invalid
+        # set the double-ended queue  
+        # append the root to the queue 
+        # set the return string 
+        # loop the queue until it is depleted 
+            # pop item from the left side of the queue 
+            # append 'None,' to the return string and continue if the node is None
+            # append the node value to the return string 
+            # append the left child to the queue 
+            # append the right child to the queue 
+        # return the string 
+        pass 
+
+    def deserialize(self, data):
+        # return None if the data is invalid 
+        # convert the string to an array with ','
+        # create the root node 
+        # create the double-ended queue
+        # append the root to the queue 
+        # set the index for the input array
+        # loop until the queue or input array is depleted
+            # pop the queue from the left
+            # create the left child node if the item of the array is valid 
+            # create the right child node if the item of the array is valid 
+        # return the head of the tree
+        pass
 ```
--- hard/Serialize_and_Deserialize_Binary_Tree/source/BFS.py
+++ hard/Serialize_and_Deserialize_Binary_Tree/source/BFS.py
@@ -8,39 +8,61 @@
 class Codec:
 
     def serialize(self, root):
+        # return empty string if the input is invalid
         if not root:
             return ''
+        # set the double-ended queue
         queue = collections.deque()
+        # append the root to the queue
         queue.append(root)
+        # set the return string
         res = ''
+        # loop the queue until it is depleted
         while queue:
+            # pop item from the left side of the queue
             node = queue.popleft()
+            # append 'None,' to the return string and continue if the node is None
             if not node:
                 res += 'None,'
                 continue
+            # append the node value to the return string
             res += str(node.val) + ','
+            # append the left child to the queue
             queue.append(node.left)
+            # append the right child to the queue
             queue.append(node.right)
+        # return the string
         return res
 
     def deserialize(self, data):
+        # return None if the data is invalid
         if not data:
             return None
+        # convert the string to an array with ','
         ls = data.split(',')
+        # create the root node
         root = TreeNode(int(ls[0]))
+        # create the double-ended queue
         queue = collections.deque()
+        # append the root to the queue
         queue.append(root)
+        # set the index for the input array
         i = 1
+        # loop until the queue or input array is depleted
         while queue and i < len(ls):
+            # pop the queue from the left
             node = queue.popleft()
+            # create the left child node if the item of the array is valid
             if ls[i] != 'None':
                 left = TreeNode(int(ls[i]))
                 node.left = left
                 queue.append(left)
             i += 1
+            # create the right child node if the item of the array is valid
             if ls[i] != 'None':
                 right = TreeNode(int(ls[i]))
                 node.right = right
                 queue.append(right)
             i += 1
+        # return the head of the tree
         return root
