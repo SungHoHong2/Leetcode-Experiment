ad03523b3a81f5aca9b273bb1a8f5a657ad7c4a4
maverickjin88 <maverickjin88@gmail.com>
2020-12-07 16:13:22 -0700

    revised medium

--- medium/Word_Search/source/backtrack.py
+++ medium/Word_Search/source/backtrack.py
@@ -13,9 +13,8 @@ class Solution:
         return False
 
     def backtrack(self, row, col, suffix):
-        # if the snake is completed
+        # return true if the snake is completed
         if len(suffix) == 0:
-            # return true
             return True
         # if the row and column exceeds the table or the target character does no create the snake
         if not(0<=row<len(self.board)) or not(0<=col<len(self.board[0])) or self.board[row][col] != suffix[0]:
@@ -23,12 +22,11 @@ class Solution:
         # mark the word as searched to prevent infinite loop
         self.board[row][col] = '#'
         # traverse the neighboring elements
-        for rowOffset, colOffset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-            # if the snake is found from the recursion
-            if self.backtrack(row + rowOffset, col + colOffset, suffix[1:]):
-                # return true
+        for directions in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            # return true if the snake is found from the recursion
+            if self.backtrack(row+directions[0], col+directions[1], suffix[1:]):
                 return True
-        # return back to original state to allow other recursions to search for the snake
+        # rest the table back to original state to allow other recursions to search
         self.board[row][col] = suffix[0]
-        # return false is no snake is found
+        # return false when no snake is found
         return False
