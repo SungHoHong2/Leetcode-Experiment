88086f319abae40cb509419a5e70d114c1913503
maverickjin88 <maverickjin88@gmail.com>
2020-12-14 13:29:00 -0700

    revise medium

--- medium/Before_and_after_Puzzle/source/UsingSets2.py
+++ medium/Before_and_after_Puzzle/source/UsingSets2.py
@@ -1,30 +1,28 @@
-### Before and After Puzzle
-**Using Sets**
-- Concepts
-    - Create a map that stores the first string as the key and the rest of the strings as the value 
-        - `writing code` = `first["writing"] = {"code"}`
-    - Create a map that stores the last string as the key and the rest of the strings as the value 
-        - `writing code` = `first["code"] = {"writing"}`
-    - Merge all of the possible overlaps using the two maps
-- [Source code v1](source/UsingSets.py)
-- [Source code v2](source/UsingSets2.py)
-
-```python
-class Solution:
-    def beforeAndAfterPuzzles(self, phrases: List[str]) -> List[str]:
 class Solution:
     def beforeAndAfterPuzzles(self, phrases: List[str]) -> List[str]:
         # create the map that stores the first string of the phrases as the key and the rest as a set
+        first = collections.defaultdict(set)
         # create the map that stores the last string of the phrases as the key and the rest as a set
+        last = collections.defaultdict(set)
         # create the return set
+        res = set()
         # iterate the phrases
+        for p in phrases:
             # split the phrase into an array of string by space
+            strs = p.split(' ')
             # if the first string of the current phrase matches with some last string of the other phrase
+            if strs[0] in last:
                 # combine the overlapping phrases {rest of the last string + last string}
+                for item in [a+p for a in last[strs[0]]]:
+                    res.add(item)
             # if the last string of the current phrase matches with the first string of the other phrase
-                # combine the overlapping phrases {first string + rest of the first string}            
+            if strs[-1] in first:
+                # combine the overlapping phrases {first string + rest of the first string}
+                for item in [p+b for b in first[strs[-1]]]:
+                    res.add(item)
             # store the first string as the key and the rest in a set
+            first[strs[0]].add(p[len(strs[0]):])
             # store the last string as the key and the rest in a set
+            last[strs[-1]].add(p[:len(p) - len(strs[-1])])
         # return with the sorted result
-        pass
-```
+        return sorted(list(res))
