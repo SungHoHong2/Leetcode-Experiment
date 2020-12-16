2d8601b8099ba17c49c170088804c6ecf4bc4e16
maverickjin88 <maverickjin88@gmail.com>
2020-12-16 11:50:58 -0700

    revise medium

--- medium/Cheapest_Flights_Within_K_Stops/README.md
+++ medium/Cheapest_Flights_Within_K_Stops/README.md
@@ -53,16 +53,13 @@ class Solution:
 ```python
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
-        # create the bellman table with rows as number of stops and cols as destinations 
+        # create the bellman table that stores number of stops as rows and cities as columns
         # set the distances of the origin node to zero
-        # K + 1 iterations of Bellman Ford
-            # update all the flights from the previous result
+        # iterate K + 1 steps
+            # update all the cost from the previous step
             # update the new distances
-                # get the total distance of the start from the previous array
-                # get the total distance of the dest from the current array
-                # if the newly added weight
-                    # update the current destination
-        # return -1 if the Kth destination is not reachable, else return the shortest distance
+                # update the minimum cost of the travel
+        # return the minimum cost or -1 if the destination is not reachable within K + 1 steps
         pass
 ```
 ```python
--- medium/Cheapest_Flights_Within_K_Stops/source/BellmanV1.py
+++ medium/Cheapest_Flights_Within_K_Stops/source/BellmanV1.py
@@ -1,24 +1,18 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
-        # create the whole array for storing distances and swapping
-        distances = [[float('inf') for i in range(n)] for j in range(K+2)]
+        # create the bellman table that stores number of stops as rows and cities as columns
+        bellman = [ [float('inf') for _ in range(n)] for _ in range(K+2)]
         # set the distances of the origin node to zero
-        distances[0][src] = 0
-        # K + 1 iterations of Bellman Ford
-        for iteration in range(1,K+2):
-            # update all the flights from the previous result
-            for s, d, w in flights:
-                distances[iteration][s]  = distances[iteration-1][s]
-                distances[iteration][d]  = distances[iteration-1][d]
+        bellman[0][src] = 0
+        # iterate K + 1 steps
+        for i in range(1, K+2):
+            # update all the cost from the previous step
+            for u,v,w in flights:
+                bellman[i][u] = bellman[i-1][u]
+                bellman[i][v] = bellman[i-1][v]
             # update the new distances
-            for s, d, w in flights:
-                # get the total distance of the start from the previous array
-                prev = distances[iteration-1][s]
-                # get the total distance of the dest from the current array
-                curr = distances[iteration][d]
-                # if the newly added weight
-                if prev + w < curr:
-                    # update the current destination
-                    distances[iteration][d] =  prev + w
-        # return -1 if the Kth destination is not reachable, else return the shortest distance
-        return -1 if distances[K+1][dst] == float("inf") else distances[K+1][dst]
+            for u,v,w in flights:
+                # update the minimum cost of the travel
+                bellman[i][v] = min(bellman[i][v], bellman[i-1][u] + w)
+        # return the minimum cost or -1 if the destination is not reachable within K + 1 steps
+        return -1 if bellman[K+1][dst] == float('inf') else bellman[K+1][dst]
