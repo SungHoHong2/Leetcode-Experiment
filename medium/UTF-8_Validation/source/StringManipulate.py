d5d9018e0ac732d38cd00ca322040e9ea8c9fa8d
maverickjin88 <maverickjin88@gmail.com>
2020-12-16 12:37:38 -0700

    revise medium

--- medium/UTF-8_Validation/source/StringManipulate.py
+++ medium/UTF-8_Validation/source/StringManipulate.py
@@ -1,29 +1,29 @@
 class Solution:
-    def validUtf8(self, data):
-        # set the variable to count the total number of grouped bytes
+    def validUtf8(self, data: List[int]) -> bool:
+        # set the variable to keep track of the number of following bytes
         n_bytes = 0
         # iterate the numbers from the input
         for num in data:
             # convert the numbers into binary representation of 8 bits
             bin_rep = format(num, '#010b')[-8:]
-            # if it is the first byte of the group
+            # if the current byte is the first byte
             if n_bytes == 0:
-                # count the total number of following bytes until zero
+                # count the total number of following bytes
                 for bit in bin_rep:
                     if bit == '0': break
                     n_bytes += 1
-                # continue the loop if the there is only one byte in the group
+                # continue if the there are no following bytes
                 if n_bytes == 0:
                     continue
-                # return false if there is only 1 or more than 4 at the first byte
+                # return false if the first byte is a following byte or has more than 4 following bytes
                 if n_bytes == 1 or n_bytes > 4:
                     return False
-            # if the group is more than 1 and the first byte is valid
+            # if the current byte is a following byte
             else:
-                # return false if the most significants 2bits are not 1,0
+                # return false if the following byte is not a following byte
                 if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
                     return False
             # decrement the total number of bytes of the group
             n_bytes -= 1
-        # return true if all the bytes in the group are processed
+        # return true if all the bytes follow the rules
         return n_bytes == 0
