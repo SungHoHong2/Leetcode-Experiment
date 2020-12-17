a740ce3e576a1ce680775e34f52e5799bec11502
maverickjin88 <maverickjin88@gmail.com>
2020-12-17 12:39:25 -0700

    revise medium

--- medium/Cheapest_Flights_Within_K_Stops/source/memoization.py
+++ medium/Cheapest_Flights_Within_K_Stops/source/memoization.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findShortest(self, city, stops, dst, n):
+    def findShortest(self, city, stops, dst):
         # return 0 if the destination is reached
         if city == dst:
             return 0
@@ -10,26 +10,26 @@ class Solution:
         if (city, stops) in self.cache:
             return self.cache[(city, stops)]
         # set a variable for recording the minimum cost
-        cost = float('inf')
+        ans = float('inf')
         # Iterate the weights of the neighbors
-        for neigh in range(n):
+        for neigh, weight in enumerate(self.adjMatrix[city]):
             # if there is a neighbor according to the adjacency matrix
-            if self.adjMatrix[city][neigh] > 0:
+            if weight > 0:
                 # get the minimal cost created from traveling to the destination
-                cost = min(cost, self.findShortest(neigh, stops - 1, dst, n) + self.adjMatrix[city][neigh])
+                ans = min(ans, self.findShortest(neigh, stops - 1, dst) + weight)
         # cache the minimum cost
-        self.cache[(city, stops)] = cost
+        self.cache[(city, stops)] = ans
         # return the minimum cost
         return self.cache[(city, stops)]
 
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
         # set an adjacency matrix that uses weights as the value
-        self.adjMatrix = [[0 for _ in range(n)] for _ in range(n)]
+        self.adjMatrix = [[0 for i in range(n)] for i in range(n)]
         for u, v, w in flights:
             self.adjMatrix[u][v] = w
-            # set a cache
+        # set a cache
         self.cache = dict()
         # return the result from the recursion
-        ans = self.findShortest(src, K + 1, dst, n)
+        ans = self.findShortest(src, K + 1, dst)
         # return the result or -1 if the result is inf
-        return ans if ans < float('inf') else -1
+        return ans if ans != float('inf') else -1
