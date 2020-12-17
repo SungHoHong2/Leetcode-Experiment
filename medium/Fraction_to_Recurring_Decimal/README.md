2c16914d627ad6899217b01884434167578c5a76
maverickjin88 <maverickjin88@gmail.com>
2020-12-16 18:41:28 -0700

    revise medium

--- medium/Fraction_to_Recurring_Decimal/README.md
+++ medium/Fraction_to_Recurring_Decimal/README.md
@@ -6,25 +6,25 @@ class Solution:
     def fractionToDecimal(self, numerator: int, denominator: int) -> str:
         # set the return string 
         # add "-" to the return if the result of the division is negative
-        # return the result directly if the result of the division divides without the remainder
-        # set the numerator and denominator as absolute
+        # return the result directly if the result has no remainder
+        # set the numerator and denominator to absolute
         # append the divided result 
         # append the dot
         # get the remainder
-        # get the current length of the string 
-        # set a map to track the repeated results 
-        # run the loop until numerator becomes zero
+        # get the starting position of the remainder
+        # set a map to track the repeating remainder 
+        # run the loop until the remainder becomes zero
             # if the current remainder is not in the table
                 # add the remainder in the table with the index of the string 
             # if the current remainder is found in the table
-                # get the index
-                # use it to wrap the repeated remainders
+                # get the staring position of the repeating number 
+                # return the result wrapped the repeated remainders
             # multiply the remainder with 10
             # add the divided result to the return string 
             # get the next remainder
             # increase the index
         # return the string
-        pass 
+        pass
 ```
 
 
--- medium/Fraction_to_Recurring_Decimal/source/LongDivision.py
+++ medium/Fraction_to_Recurring_Decimal/source/LongDivision.py
@@ -5,41 +5,40 @@ class Solution:
         # add "-" to the return if the result of the division is negative
         if numerator / denominator < 0:
             res += "-"
-        # return the result directly if the result of the division divides without the remainder
+        # return the result directly if the result has no remainder
         if numerator % denominator == 0:
             return str(numerator // denominator)
-        # set the numerator and denominator as absolute
+        # set the numerator and denominator to absolute
         numerator = abs(numerator)
         denominator = abs(denominator)
         # append the divided result
         res += str(numerator // denominator)
         # append the dot
-        res += "."
+        res += '.'
         # get the remainder
-        numerator %= denominator
-        # get the current length of the string
+        remainder = numerator % denominator
+        # get the starting position of the remainder
         i = len(res)
-        # set a map to track the repeated results
-        table = {}
-        # run the loop until numerator becomes zero
-        while numerator != 0:
+        # set a map to track the repeating remainder
+        table = dict()
+        # run the loop until the remainder becomes zero
+        while remainder != 0:
             # if the current remainder is not in the table
-            if numerator not in table.keys():
+            if remainder not in table:
                 # add the remainder in the table with the index of the string
-                table[numerator] = i
+                table[remainder] = i
             # if the current remainder is found in the table
             else:
-                # get the index
-                i = table[numerator]
-                # use it to wrap the repeated remainders
-                res = res[:i] + "(" + res[i:] + ")"
-                return res
+                # get the staring position of the repeating number
+                i = table[remainder]
+                # return the result wrapped the repeated remainders
+                return res[:i] + "(" + res[i:] + ")"
             # multiply the remainder with 10
-            numerator = numerator * 10
+            remainder = remainder * 10
             # add the divided result to the return string
-            res += str(numerator // denominator)
+            res += str(remainder // denominator)
             # get the next remainder
-            numerator %= denominator
+            remainder %= denominator
             # increase the index
             i += 1
         # return the string
