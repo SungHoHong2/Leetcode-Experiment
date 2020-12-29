3642267195ad384f2a0febc3f1926f73e424384d
maverickjin88 <maverickjin88@gmail.com>
2020-12-28 18:26:48 -0700

    revise medium

--- medium/3Sum/README.md
+++ medium/3Sum/README.md
@@ -16,6 +16,7 @@ class Solution:
             # if the first element or the neighboring element is not the same 
                 # invoke the twoPointer function
         # return the answers
+        pass
     
     def twoSum2(self, nums: List[int], i: int, res: List[List[int]]):
         # get the 3 indexes (current, leftmost from the current, and the rightmost indexes) 
@@ -29,6 +30,7 @@ class Solution:
                 # add the answers to the return array 
                 # reduce the range of the left and the right index 
                 # loop until left element is not a duplicate of the previous element
+        pass
 ```
 
 **Hashset**
@@ -44,6 +46,7 @@ class Solution:
             # if the element is the first or not duplicated
                 # invoke the twoSum function
         # return the result
+        pass
 
     def twoSum(self, nums: List[int], i: int, res: List[List[int]]):
         # create a hashset
@@ -57,6 +60,7 @@ class Solution:
                 # add the answer to the return list
                 # increase the leftmost index
                 # skip the leftmost indexes that are duplicates
+        pass
 ```
 
 **No-Sort**
@@ -66,14 +70,15 @@ class Solution:
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         # create a set for return value and checking for duplicates
-        # create a hashmap
         # iterate the numbers
             # if the variable are not in the duplicate
+                # create a hashmap
                 # add the variable in the hashset
                 # iterate the rightside array of the current element
                     # calculate the complement
                     # if the complement is in the hashmap and it is part of the current index
                         # add the results(fix the orders because the answer may append duplicates)
-                    # store the leftmost value as the complement(key) and the current element as the value in the hashmap
+                    # add the leftmost value in the hashmap and the current index as the key
         # return the array
+        pass
 ```
--- medium/3Sum/source/hashset.py
+++ medium/3Sum/source/hashset.py
@@ -1,43 +1,43 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # set the return array
+        # set the return array that contains the elements that sum up to zero
         res = list()
-        # assume that the array is already sorted
+        # assume that the input array is already sorted
         nums.sort()
-        # iterate the array
+        # iterate the numbers
         for i in range(len(nums)):
-            # if the current element is above zero
+            # if the twoSum2 cannot produce zero anymore
             if nums[i] > 0:
-                # stop the iteration
+                # finish iteration
                 break
-                # if the element is the first or not duplicated
+            # if the first element or the neighboring element is not the same
             if i == 0 or nums[i - 1] != nums[i]:
-                # invoke the twoSum function
-                self.twoSum(nums, i, res)
-        # return the result
+                # invoke the twoPointer function
+                self.twoSum2(nums, i, res)
+        # return the answers
         return res
 
-    def twoSum(self, nums: List[int], i: int, res: List[List[int]]):
+    def twoSum2(self, nums: List[int], i: int, res: List[List[int]]):
         # create a hashset
-        hashset = set()
+        cache = set()
         # set the index of the leftmost element that is at the rightside of the current element
-        left = i + 1
-        # iterate the array
-        while left < len(nums):
+        left, right = i + 1, len(nums)
+        # loop until the indexes converge
+        while left < right:
             # calculate the complement
             complement = 0 - nums[i] - nums[left]
             # if the complement is not in the memory
-            if complement not in hashset:
+            if complement not in cache:
                 # record the current element
-                hashset.add(nums[left])
+                cache.add(nums[left])
                 # increase the leftmost index
                 left += 1
-                # if the complement is in the hashset
+                # if the complement is in the memory
             else:
                 # add the answer to the return list
                 res.append([nums[i], nums[left], complement])
                 # increase the leftmost index
                 left += 1
                 # skip the leftmost indexes that are duplicates
-                while left < len(nums) and nums[left - 1] == nums[left]:
+                while left < right and nums[left - 1] == nums[left]:
                     left += 1
