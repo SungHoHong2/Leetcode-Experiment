915a6d03a738c856a44655705ae783e313b7c17b
maverickjin88 <maverickjin88@gmail.com>
2021-01-04 14:45:38 -0700

    revise medium

--- medium/Subarray_Sum_Equals_K/source/Hashmap.py
+++ medium/Subarray_Sum_Equals_K/source/Hashmap.py
@@ -1,28 +1,20 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         # initialize a hashmap
-        hashmap = {}
+        hashmap = collections.defaultdict(int)
         # set the variables for counter and the sum
         count, sum = 0, 0
         # count zero in hashmap
         hashmap[0] = 1
         # iterate the numbers
         for i in nums:
-            # accumulate the sum
+            # expand the size of the contiguous subset
             sum += i
-            # if the sum subtracted from k is found in hashmap
-            if ((sum - k) in hashmap):
-                # subarray sum of k exists so increase the number of counts
-                count += hashmap[sum - k]
-            # if the sum is found again in the hashmap
-            if (sum in hashmap):
-                # increase the number of counts
-                hashmap[sum] += 1
-            # if the sum does not exist in the hashmap
-            else:
-                # start counting the sum in hashmap
-                hashmap[sum] = 1
+            # if the contiguous subset that sums up to k is found
+            if (sum-k) in hashmap:
+                # aggregate the number of subsets that can be equal to k
+                count += hashmap[sum-k]
+            # record the number of the contiguous subsets that equals to a certain number
+            hashmap[sum] += 1
         # return the number of counts
         return count
-
-
