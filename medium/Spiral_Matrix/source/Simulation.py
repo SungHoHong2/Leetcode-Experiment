5afeb1441814b80f82c9398a5434078ca1cbd67a
maverickjin88 <maverickjin88@gmail.com>
2021-01-05 14:30:27 -0700

    revise medium

--- medium/Spiral_Matrix/source/Simulation.py
+++ medium/Spiral_Matrix/source/Simulation.py
@@ -3,36 +3,34 @@ class Solution(object):
         # if the matrix is empty
         if not matrix:
             # return a empty row
-            return []
+            return list()
         # get the number of rows and cols
-        R, C = len(matrix), len(matrix[0])
+        rows, cols = len(matrix), len(matrix[0])
         # create the matrix for checking the visited cells
-        seen = [[False for i in range(C)] for j in range(R)]
+        visited = [[0 for c in range(cols)] for r in range(rows)]
         # initialize a list for return
-        ans = []
-        # set the order of direction for the row (down,up)
-        dr = [0, 1, 0, -1]
-        # set the order of direction for the column (right,left)
-        dc = [1, 0, -1, 0]
-        # initalize the pointers for the direction
-        r = c = di = 0
+        ans = list()
+        # set the order of direction (right,down,left,up)
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        # initialize the index for the matrix and the direction
+        r,c,d = 0,0,0
         # iterate the total number of cells in the matrix
-        for _ in range(R * C):
+        for _ in range(rows * cols):
             # append the current cell
             ans.append(matrix[r][c])
             # set the current cell visited
-            seen[r][c] = True
+            visited[r][c] = 1
             # move the current cell using the current direction (right,down,left,up)
-            cr, cc = r + dr[di], c + dc[di]
+            i, j = r + directions[d][0], c + directions[d][1]
             # if the cell is within the matrix and it is not visited
-            if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:
+            if 0<=i<rows and 0<=j<cols and not visited[i][j]:
                 # follow the current direction
-                r, c = cr, cc
+                r, c = i, j
             # if the cell exceeds the matrix
             else:
                 # change the direction
-                di = (di + 1) % 4
+                d = (d + 1) % 4
                 # update the current cell
-                r, c = r + dr[di], c + dc[di]
+                r, c = r + directions[d][0], c + directions[d][1]
         # return the answer
         return ans
