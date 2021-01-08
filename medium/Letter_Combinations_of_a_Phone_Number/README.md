56dd9880393a221a9a7deb2005bcdddf3deb7a95
maverickjin88 <maverickjin88@gmail.com>
2021-01-08 16:41:51 -0700

    revise medium

--- medium/Letter_Combinations_of_a_Phone_Number/README.md
+++ medium/Letter_Combinations_of_a_Phone_Number/README.md
@@ -6,15 +6,17 @@
 - [Source code](source/Backtrack.py)
 ```python
 class Solution:
-    def backtrack(self, combination, next_digits):
-        # append the combination to the return list if the input is depleted
-        # if there are still digits to check
-            # iterate the letters which are mapped to the number
-                # provide the combination and the next digit to the next recursive tree
+    def recursion(self, combination, next_digits):
+        # set a return array that stores all the combinations 
+        # return the combination to the return list if the input is depleted
+        # iterate the letters which are mapped to the number
+            # extend the combinations to the return array 
+        # return the combinations
         pass
 
-    def letterCombinations(self, digits):
-        # set the map for all the characters mapped to the number
+    def letterCombinations(self, digits: str) -> List[str]:
+        # return an empty list if there is no input 
+        # create a dictionary that maps all the letters with the numbers
         self.phone = {'2': ['a', 'b', 'c'],
                       '3': ['d', 'e', 'f'],
                       '4': ['g', 'h', 'i'],
@@ -22,10 +24,7 @@ class Solution:
                       '6': ['m', 'n', 'o'],
                       '7': ['p', 'q', 'r', 's'],
                       '8': ['t', 'u', 'v'],
-                      '9': ['w', 'x', 'y', 'z']}
-        # set the answer as an array
-        # return the answer if there is no input
-        # invoke the recursive function
-        # return the list
+                      '9': ['w', 'x', 'y', 'z']}        
+        # return the combination from the recursion 
         pass
 ```
--- medium/Letter_Combinations_of_a_Phone_Number/source/Backtrack.py
+++ medium/Letter_Combinations_of_a_Phone_Number/source/Backtrack.py
@@ -1,17 +1,23 @@
 class Solution:
-    def backtrack(self, combination, next_digits):
-        # append the combination to the return list if the input is depleted
-        if len(next_digits) == 0:
-            self.output.append(combination)
-        # if there are still digits to check
-        else:
-            # iterate the letters which are mapped to the number
-            for letter in self.phone[next_digits[0]]:
-                # provide the combination and the next digit to the next recursive tree
-                self.backtrack(combination + letter, next_digits[1:])
 
-    def letterCombinations(self, digits):
-        # set the map for all the characters mapped to the number
+    def recursion(self, combination, next_digits):
+        # set a return array that stores all the combinations
+        ans = list()
+        # return the combination to the return list if the input is depleted
+        if not next_digits:
+            return [combination]
+        # iterate the letters which are mapped to the number
+        for c in self.phone[next_digits[0]]:
+            # extend the combinations to the return array
+            ans += self.recursion(combination + c, next_digits[1:])
+        # return the combinations
+        return ans
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        # return an empty list if there is no input
+        if not digits:
+            return list()
+        # create a dictionary that maps all the letters with the numbers
         self.phone = {'2': ['a', 'b', 'c'],
                       '3': ['d', 'e', 'f'],
                       '4': ['g', 'h', 'i'],
@@ -20,12 +26,5 @@ class Solution:
                       '7': ['p', 'q', 'r', 's'],
                       '8': ['t', 'u', 'v'],
                       '9': ['w', 'x', 'y', 'z']}
-        # set the answer as an array
-        self.output = list()
-        # return the answer if there is no input
-        if not digits:
-            return self.output
-        # invoke the recursive function
-        self.backtrack("", digits)
-        # return the list
-        return self.output
+        # return the combination from the recursion
+        return self.recursion("", digits)
