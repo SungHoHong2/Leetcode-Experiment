0b12c72f95f94a4518eed0ac96ad27575031ff1d
maverickjin88 <maverickjin88@gmail.com>
2021-01-11 11:22:00 -0700

    revise medium

--- medium/Word_Break/source/Dp.py
+++ medium/Word_Break/source/Dp.py
@@ -3,13 +3,13 @@ class Solution(object):
         # set up the dp table
         dp = [False for _ in s]
         # iterate the string
-        for end in range(len(s)):
-            # interate the substring backwards
-            for start in range(end+1,-1,-1):
+        for end in range(1,len(s)+1):
+            # iterate the substring backwards
+            for start in range(end, -1, -1):
                 # if the current word is valid and the previous substrings are valid
-                if s[start:end+1] in wordDict and (dp[start-1] if start > 0 else True):
+                if s[start:end] in wordDict and (dp[start-1] if start > 0 else True):
                     # set the dp table to true
-                    dp[end] = True
+                    dp[end-1] = True
                     # ignore the rest of the substrings
                     break
         # return the final result of the record
