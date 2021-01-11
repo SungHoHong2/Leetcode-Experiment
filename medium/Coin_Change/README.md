f4f6963e18b1e5cce744eedba38a4178bb935c78
maverickjin88 <maverickjin88@gmail.com>
2021-01-10 19:40:02 -0700

    revise medium

--- medium/Coin_Change/README.md
+++ medium/Coin_Change/README.md
@@ -6,20 +6,20 @@
 ```python
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # create the cache to record the results
+        # set up the dp table to record the number of required coins according to the amount 
         # base case: when the amount is zero then amount of required coins is zero
-        # invoke the recursion
+        # collect the answer from the top down
+        # return the answer or -1 if the coins do not sum up to the amount 
         pass
        
     def topdown(self, coins, remain):
-        # return false if there is no matching accumulation of coins  
-        # return the recorded result if the answer is already explored
-        # set the min variable to get the minimum collection of coins to match the amount
+        # return infinite if the coins do not sum up to the amount
+        # return the recorded result if the answer is cached in the dp table 
+        # set the variable to record the minimum number of coins
         # iterate the coins
-            # get the number of possible coins from the recursion
-            # Update the min value if the result is valid (note that need to + 1 for the final result)
-        # update the minimal number of coins that fits into the certain amount
-        # return the number of coins that are required to match the amount
+            # get the smallest number of coins that sum up to the remaining amount
+        # update the number of coins that sum up to the amount in the dp table 
+        # return result
         pass
 ```
 
--- medium/Coin_Change/source/Topdown.py
+++ medium/Coin_Change/source/Topdown.py
@@ -1,29 +1,28 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # create the cache to record the results
+        # set up the dp table to record the number of required coins according to the amount
         self.dp = [None for i in range(amount + 1)]
         # base case: when the amount is zero then amount of required coins is zero
         self.dp[0] = 0
-        # invoke the recursion
-        return self.topdown(coins, amount)
+        # collect the answer from the top down
+        res = self.topdown(coins, amount)
+        # return the answer or -1 if the coins do not sum up to the amount
+        return res if res != float('inf') else -1
 
     def topdown(self, coins, remain):
-        # return false if there is no matching accumulation of coins
+        # return infinite if the coins do not sum up to the amount
         if remain < 0:
-            return -1
-            # return the recorded result if the answer is already explored
+            return float('inf')
+        # return the recorded result if the answer is cached in the dp table
         if self.dp[remain] != None:
             return self.dp[remain]
-        # set the min variable to get the minimum collection of coins to match the amount
+        # set the variable to record the minimum number of coins
         ans = float('inf')
         # iterate the coins
         for coin in coins:
-            # get the number of possible coins from the recursion
-            res = self.topdown(coins, remain - coin)
-            # Update the min value if the result is valid (note that need to + 1 for the final result)
-            if res != -1 and res < ans:
-                ans = 1 + res
-        # update the minimal number of coins that fits into the certain amount
-        self.dp[remain] = -1 if ans == float('inf') else ans
-        # return the number of coins that are required to match the amount
+            # get the smallest number of coins that sum up to the remaining amount
+            ans = min(ans, self.topdown(coins, remain - coin))
+        # update the number of coins that sum up to the amount in the dp table
+        self.dp[remain] = ans + 1
+        # return result
         return self.dp[remain]
