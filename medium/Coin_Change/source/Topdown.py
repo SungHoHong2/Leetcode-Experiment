ef2dd1550d68724fa3e06ad5564f5b9597cebbe8
maverickjin88 <maverickjin88@gmail.com>
2021-01-10 19:03:47 -0700

    revise medium

--- medium/Coin_Change/source/Topdown.py
+++ medium/Coin_Change/source/Topdown.py
@@ -15,15 +15,15 @@ class Solution:
         if self.dp[remain] != None:
             return self.dp[remain]
         # set the min variable to get the minimum collection of coins to match the amount
-        min = float('inf')
+        ans = float('inf')
         # iterate the coins
         for coin in coins:
             # get the number of possible coins from the recursion
             res = self.topdown(coins, remain - coin)
             # Update the min value if the result is valid (note that need to + 1 for the final result)
-            if 0 <= res < min:
-                min = 1 + res
-                # update the minimal number of coins that fits into the certain amount
-        self.dp[remain] = -1 if min == float('inf') else min
+            if res != -1 and res < ans:
+                ans = 1 + res
+        # update the minimal number of coins that fits into the certain amount
+        self.dp[remain] = -1 if ans == float('inf') else ans
         # return the number of coins that are required to match the amount
         return self.dp[remain]
