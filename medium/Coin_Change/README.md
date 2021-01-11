20b10680d15b143f57c69a0cf59eeaf64986a033
maverickjin88 <maverickjin88@gmail.com>
2021-01-11 13:42:54 -0700

    revise medium

--- medium/Coin_Change/README.md
+++ medium/Coin_Change/README.md
@@ -6,19 +6,20 @@
 ```python
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # set the coins as a global variable 
         # set up the dp table to record the number of required coins according to the amount 
         # base case: when the amount is zero then amount of required coins is zero
         # collect the answer from the top down
         # return the answer or -1 if the coins do not sum up to the amount 
         pass
        
-    def topdown(self, coins, remain):
+    def topdown(self, dp, remain):
         # return infinite if the coins do not sum up to the amount
         # return the recorded result if the answer is cached in the dp table 
         # set the variable to record the minimum number of coins
         # iterate the coins
             # get the smallest number of coins that sum up to the remaining amount
-        # update the number of coins that sum up to the amount in the dp table 
+        # update the number of coins that sum up to the amount in the dp table
         # return result
         pass
 ```
--- medium/Coin_Change/source/Topdown.py
+++ medium/Coin_Change/source/Topdown.py
@@ -1,28 +1,30 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # set the coins as a global variable
+        self.coins = coins
         # set up the dp table to record the number of required coins according to the amount
-        self.dp = [None for i in range(amount + 1)]
+        dp = [None for _ in range(amount + 1)]
         # base case: when the amount is zero then amount of required coins is zero
-        self.dp[0] = 0
+        dp[0] = 0
         # collect the answer from the top down
-        res = self.topdown(coins, amount)
+        ans = self.topdown(dp, amount)
         # return the answer or -1 if the coins do not sum up to the amount
-        return res if res != float('inf') else -1
+        return ans if ans != float('inf') else -1
 
-    def topdown(self, coins, remain):
+    def topdown(self, dp, remain):
         # return infinite if the coins do not sum up to the amount
         if remain < 0:
             return float('inf')
         # return the recorded result if the answer is cached in the dp table
-        if self.dp[remain] != None:
-            return self.dp[remain]
+        if dp[remain] != None:
+            return dp[remain]
         # set the variable to record the minimum number of coins
         ans = float('inf')
         # iterate the coins
-        for coin in coins:
+        for coin in self.coins:
             # get the smallest number of coins that sum up to the remaining amount
-            ans = min(ans, self.topdown(coins, remain - coin))
+            ans = min(ans, self.topdown(dp, remain - coin))
         # update the number of coins that sum up to the amount in the dp table
-        self.dp[remain] = ans + 1
+        dp[remain] = ans + 1
         # return result
-        return self.dp[remain]
+        return dp[remain]
