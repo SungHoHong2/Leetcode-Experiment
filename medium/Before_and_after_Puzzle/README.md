30c4ebc29d8f7d559e8a4cdba28afce8ccbba054
maverickjin88 <maverickjin88@gmail.com>
2021-01-12 12:16:00 -0700

    revise medium

--- medium/Before_and_after_Puzzle/README.md
+++ medium/Before_and_after_Puzzle/README.md
@@ -12,17 +12,15 @@
 ```python
 class Solution:
     def beforeAndAfterPuzzles(self, phrases: List[str]) -> List[str]:
-        # create the map that stores the first string of the phrases as the key and the rest as a set
-        # create the map that stores the last string of the phrases as the key and the rest as a set
+        # create the map that stores the first string of the phrases as the key 
+        # create the map that stores the last string of the phrases as the key 
         # create the return set
         # iterate the phrases
             # split the phrase into an array of string by space
-            # if the first string of the current phrase matches with some last string of the other phrase
-                # combine the overlapping phrases {rest of the last string + last string}
-            # if the last string of the current phrase matches with the first string of the other phrase
-                # combine the overlapping phrases {first string + rest of the first string}            
-            # store the first string as the key and the rest in a set
-            # store the last string as the key and the rest in a set
+            # combine the phrases that matches with the set of the last string
+            # combine the phrases that matches with the set of the first string
+            # store the first string as the key and add the rest to the set
+            # store the last string as the key and add the rest to the set
         # return with the sorted result
         pass
 ```
--- medium/Before_and_after_Puzzle/source/UsingSets2.py
+++ medium/Before_and_after_Puzzle/source/UsingSets2.py
@@ -1,8 +1,8 @@
 class Solution:
     def beforeAndAfterPuzzles(self, phrases: List[str]) -> List[str]:
-        # create the map that stores the first string of the phrases as the key and the rest as a set
+        # create the map that stores the first string of the phrases as the key
         first = collections.defaultdict(set)
-        # create the map that stores the last string of the phrases as the key and the rest as a set
+        # create the map that stores the last string of the phrases as the key
         last = collections.defaultdict(set)
         # create the return set
         res = set()
@@ -10,19 +10,15 @@ class Solution:
         for p in phrases:
             # split the phrase into an array of string by space
             strs = p.split(' ')
-            # if the first string of the current phrase matches with some last string of the other phrase
-            if strs[0] in last:
-                # combine the overlapping phrases {rest of the last string + last string}
-                for item in [a+p for a in last[strs[0]]]:
-                    res.add(item)
-            # if the last string of the current phrase matches with the first string of the other phrase
-            if strs[-1] in first:
-                # combine the overlapping phrases {first string + rest of the first string}
-                for item in [p+b for b in first[strs[-1]]]:
-                    res.add(item)
-            # store the first string as the key and the rest in a set
+            # combine the phrases that matches with the set of the last string
+            for item in [a+p for a in last[strs[0]]]:
+                res.add(item)
+            # combine the phrases that matches with the set of the first string
+            for item in [p+b for b in first[strs[-1]]]:
+                res.add(item)
+            # store the first string as the key and add the rest to the set
             first[strs[0]].add(p[len(strs[0]):])
-            # store the last string as the key and the rest in a set
-            last[strs[-1]].add(p[:len(p) - len(strs[-1])])
+            # store the last string as the key and add the rest to the set
+            last[strs[-1]].add(p[:len(p)-len(strs[-1])])
         # return with the sorted result
         return sorted(list(res))
