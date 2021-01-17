a71bf5a929a3e7263649bd8f83f5cfa9fef28bf9
maverickjin88 <maverickjin88@gmail.com>
2021-01-17 13:22:54 -0700

    revise medium

--- hard/Maximum_Profit_in_Job_Scheduling/source/TopDown.py
+++ hard/Maximum_Profit_in_Job_Scheduling/source/TopDown.py
@@ -4,6 +4,7 @@ class Job:
         self.finish = finish
         self.profit = profit
 
+
 class Solution:
     def getNonConflicting(self, jobs, n):
         # search the job that ends earlier than the current job
@@ -11,34 +12,34 @@ class Solution:
             # return the index of the latest job that does not conflict with the current job
             if jobs[i].finish <= jobs[n].start:
                 return i
-        # return -1 if every job is conflicting
+                # return -1 if every job is conflicting
         return -1
 
     def sortScheduling(self, jobs, j):
         # return 0 if the recursion requests for incompatible jobs
         if j == -1:
             return 0
-        # return the current profit if the recursive tree reached the leaf
+            # return the current profit if the recursive tree reached the leaf
         if j == 0:
             return jobs[j].profit
         # if the result of the current job is not recorded
-        if not self.M[j]:
+        if not self.dp[j]:
             # find the compatible job
-            compatible = self.getNonConflicting(jobs,j)
+            compatible = self.getNonConflicting(jobs, j)
             # record the maximum possible profit for the current job
-            self.M[j] = max(jobs[j].profit + self.sortScheduling(jobs, compatible), self.sortScheduling(jobs,j-1))
+            self.dp[j] = max(jobs[j].profit + self.sortScheduling(jobs, compatible), self.sortScheduling(jobs, j - 1))
         # return the maximum possible profit for the current job
-        return self.M[j]
+        return self.dp[j]
 
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        # initialize a list to store jobs (start, finish, profit)
+        # initialize a list to store jobs
         jobs = list()
-        # append all the jobs
+        # append all the jobs (start, finish, profit)
         for i in range(len(profit)):
             jobs.append(Job(startTime[i], endTime[i], profit[i]))
         # sort the jobs by finish time
-        jobs.sort(key=lambda x:x.finish)
+        jobs.sort(key=lambda x: x.finish)
         # initialize a map that records the maximum profit for each jobs
-        self.M = [0 for i in range(len(jobs))]
+        self.dp = [0 for _ in jobs]
         # run the recursvie tree from top to bottom
-        return self.sortScheduling(jobs,len(jobs)-1)
+        return self.sortScheduling(jobs, len(jobs) - 1)
