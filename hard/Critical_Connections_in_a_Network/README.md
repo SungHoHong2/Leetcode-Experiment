cb0d9151fddba4abb690c342782545675d82483f
maverickjin88 <maverickjin88@gmail.com>
2021-01-18 14:38:34 -0700

    revise medium

--- hard/Critical_Connections_in_a_Network/README.md
+++ hard/Critical_Connections_in_a_Network/README.md
@@ -1,6 +1,33 @@
 ### Critical Connections in a Network
 **Tarjan's bridge-finding algorithm**
 - [Concepts](images/tarjan.png)
-- [Source code](source/)
+- [Source code](source/tarjan.py)
 - [Reference #1](https://www.youtube.com/watch?v=jFZsDDB0-vo)
 - [Reference #2](https://www.youtube.com/watch?v=RYaakWv5m6o)
+
+```python
+class Solution:
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        # set the hashmap for storing the list of undirected src and dests
+        # set the pointer for tracking the order of discovery
+        # set the hashmap to record the minimum rank that the nodes can reach      
+        # set the table to mark the visited nodes 
+        # build an undirected graph 
+        # set the list for returning the critical edges
+        # set the pointer for the previous vertex
+        # set the pointer for the current vertex
+        # start the dfs
+        # return the critical edges 
+        pass
+        
+    def dfs(self, res, rank, prev, current):
+        # mark the node as visited 
+        # set the minimum rank
+        # explore the neighbors 
+            # if the vertex is not visited
+                # explore the neighbors with dfs 
+            # if the node did not reach the deadend
+                # update the lowest rank reachable once formed a cycle
+                # append the critical edge if the edge does not return as a cycle
+        pass
+```
--- hard/Critical_Connections_in_a_Network/source/tarjan.py
+++ hard/Critical_Connections_in_a_Network/source/tarjan.py
@@ -1,19 +1,19 @@
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-        # set the hashamp for storing the edges
+        # set the hashmap for storing the list of undirected src and dests
         self.graph = collections.defaultdict(list)
-        ## set the pointer for tracking the order of current rank
+        # set the pointer for tracking the order of discovery
         rank = 0
-        # set the hashamp to record the minimum rank that the nodes can reach
+        # set the hashmap to record the minimum rank that the nodes can reach
         self.lowestRank = collections.defaultdict(int)
         # set the table to mark the visited nodes
         self.visited = [False for _ in range(n)]
-        ## build graph using the hashmap
-        for connection in connections:
-            self.graph[connection[0]].append(connection[1])
-            self.graph[connection[1]].append(connection[0])
+        # build an undirected graph
+        for u, v in connections:
+            self.graph[u].append(v)
+            self.graph[v].append(u)
         # set the list for returning the critical edges
-        res = []
+        res = list()
         # set the pointer for the previous vertex
         prev = -1
         # set the pointer for the current vertex
@@ -24,7 +24,6 @@ class Solution:
         return res
 
     def dfs(self, res, rank, prev, current):
-        print('dfs', {'current': current, 'rank': rank})
         # mark the node as visited
         self.visited[current] = True
         # set the minimum rank
@@ -34,13 +33,11 @@ class Solution:
             # if the vertex is not visited
             if not self.visited[next]:
                 # explore the neighbors with dfs
-                # print('explored',{'current': current, 'next':next, 'rank': rank })
                 self.dfs(res, rank + 1, current, next)
-            # do not go to the previous vertex
+            # if the node did not reach the deadend
             if next != prev:
-                # print('update',{'current':(current,self.lowestRank[current]), 'next':(next ,self.lowestRank[next])})
-                # update the lowest rank reachable
+                # update the lowest rank reachable once formed a cycle
                 self.lowestRank[current] = min(self.lowestRank[current], self.lowestRank[next])
-                # the edge is not the cycle if the current rank is no longer compatible with the lowest reachable rank
-                if self.lowestRank[next] >= rank + 1:
+                # add critical edge if the edge does not return as a cycle
+                if self.lowestRank[next] > rank:
                     res.append([current, next])
