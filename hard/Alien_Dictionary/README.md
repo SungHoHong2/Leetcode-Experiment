c94c7dcc611f7bdf14be5e12dafe4f5b4c69e852
maverickjin88 <maverickjin88@gmail.com>
2021-01-20 14:09:36 -0700

    revise hard

--- hard/Alien_Dictionary/README.md
+++ hard/Alien_Dictionary/README.md
@@ -11,9 +11,42 @@
     - Filter the letters with no incoming links
     - Start the BFS search from the nodes with zero incoming links
 - [Source code](source/BFS.py)
+```python
+from collections import defaultdict, deque
+
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        # set a hashmap accepting set as a value
+        # set a hashmap to track of the number of incoming nodes for each char
+        # iterate each pair of adjacent words
+            # set a flag to check if the neighbors are identical
+            # iterate each pair of characters from the adjacent words
+                # if the character is different
+                    # if the character from the second word is not part of the adjacency hashmap
+                        # store the character in the adjacency list
+                        # increase the number of incoming nodes
+                    # mark as identical found
+                    # break because there is no way to identify the order beyond this
+            # if all the compared words are identical 
+                # return empty string if the second word is a subset of the first word
+        # set the list to collect the order of characters
+        # create a BFS queue that contains nodes without incoming nodes
+        # run the BFS
+            # pop out the node 
+            # append as the answer
+            # explore the destinations of the current node
+                # decrease the incoming node of the destination  
+                # append the dest to the BFS queue if the number of incoming nodes is zero 
+        # return the empty string if there are disconnected graphs 
+        # return the order of the characters as a string
+        pass
+```
 
 **DFS**
 - Concepts
     - Do not need to count the incoming links 
     - Detect cycles by using graph coloring 
 - [Source code](source/DFS.py)
+```python
+
+```
--- hard/Alien_Dictionary/source/BFS.py
+++ hard/Alien_Dictionary/source/BFS.py
@@ -1,42 +1,53 @@
-from collections import defaultdict, Counter, deque
+from collections import defaultdict, deque
 
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
         # set a hashmap accepting set as a value
         adj_list = defaultdict(set)
-        # set a hashmap to track of the number of incoming nodes
-        in_degree = {c : 0 for word in words for c in word}
+        # set a hashmap to track of the number of incoming nodes for each char
+        incoming = {char : 0 for word in words for char in word}
         # iterate each pair of adjacent words
-        for first_word, second_word in zip(words, words[1:]):
+        for first, second in zip(words, words[1:]):
+            # set a flag to check if the neighbors are identical
+            identical = True
             # iterate each pair of characters from the adjacent words
-            for c, d in zip(first_word, second_word):
+            for src, dest in zip(first, second):
                 # if the character is different
-                if c != d:
+                if src != dest:
                     # if the character from the second word is not part of the adjacency hashmap
-                    if d not in adj_list[c]:
+                    if dest not in adj_list[src]:
                         # store the character in the adjacency list
-                        adj_list[c].add(d)
+                        adj_list[src].add(dest)
                         # increase the number of incoming nodes
-                        in_degree[d] += 1
+                        incoming[dest] += 1
+                    # mark as identical found
+                    identical = False
+                    # break because there is no way to identify the order beyond this
                     break
-            # return empty string if the second word is a subset of the first word
-            else:
-                if len(second_word) < len(first_word): return ""
+            # if all the compared words are identical
+            if identical:
+                # return empty string if the second word is a subset of the first word
+                if len(second) < len(first):
+                    return ""
         # set the list to collect the order of characters
-        output = []
-        # create a double ended queue and add the starting nodes
-        queue = deque([c for c in in_degree if in_degree[c] == 0])
-        # run the BFS and append the order of characters
+        output = list()
+        # create a BFS queue that contains nodes without incomming nodes
+        queue = deque([src for src in incoming if incoming[src] == 0])
+        # run the BFS
         while queue:
-            c = queue.popleft()
-            output.append(c)
-            for d in adj_list[c]:
-                in_degree[d] -= 1
-                if in_degree[d] == 0:
-                    queue.append(d)
-
-        # return the empty string if there are missing characters in the result
-        if len(output) < len(in_degree):
+            # pop out the node
+            src = queue.popleft()
+            # append as the answer
+            output.append(src)
+            # explore the destinations of the current node
+            for dest in adj_list[src]:
+                # decrease the incomming node of the destination
+                incoming[dest] -= 1
+                # append the dest to the BFS queue if the number of incomming nodes is zero
+                if incoming[dest] == 0:
+                    queue.append(dest)
+        # return the empty string if there are disconnected graphs
+        if len(output) < len(incoming):
             return ""
-        # return the result as a string
+        # return the order of the characters as a string
         return "".join(output)
