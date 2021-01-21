50a1dbee2e2622918350e1727b9c745a86ade5e0
maverickjin88 <maverickjin88@gmail.com>
2021-01-20 17:55:21 -0700

    revise hard

--- hard/Text_Justification/README.md
+++ hard/Text_Justification/README.md
@@ -8,15 +8,20 @@ class Solution:
     def fullJustify(self, words, maxWidth):
         # set the return list, list of words, and the total length of words in the current line
         # iterate the words
-            # the exceed the maxWidth
-                # subtract the maxWidth from the number of letters
-                    # increase the number of blanks to each word in round robin
-                    # append the result to the return array
+            # get the one extra (end of the sentence) number of white spaces 
+            # get the length of the new word
+            # if adding a new word with *extra one* more white spaces exceeds the maxWidth  
+                # iterate up the empty spaces in the line 
+                    # push the white spaces to the right if the line has a single word 
+                    # distribute the white spaces in RR if there are multiple word in the line
+                # append the sentence to the return array
                 # reset the list of words and total number of chars in each line
             # append the word for the current line
             # increase the length of the characters in the current line
-
-        # create a left-aligned sentece for the last line
-        # return the result
+        # create a left-aligned sentence for the last line
+            # distribute the white spaces in RR once 
+            # after that push all the other white spaces to the right
+        # append the sentence to the return array
+        # return the justified paragraph
         pass
 ```
--- hard/Text_Justification/source/Solution.py
+++ hard/Text_Justification/source/Solution.py
@@ -1,31 +1,39 @@
 class Solution:
     def fullJustify(self, words, maxWidth):
         # set the return list, list of words, and the total length of words in the current line
-        res, cur, num_of_letters = [], [], 0
+        res, line, line_length = list(), list(), 0
         # iterate the words
         for w in words:
-            # the exceed the maxWidth
-            if num_of_letters + len(w) + len(cur) > maxWidth:
-                # subtract the maxWidth from the number of letters
-                for i in range(maxWidth - num_of_letters):
-                    # increase the number of blanks to each word in round robin
-                    cur[0 if len(cur) - 1 == 0 else i % (len(cur) - 1)] += ' '
-                    # append the result to the return array
-                res.append(''.join(cur))
+            # get the number of potential white spaces
+            new_spaces = len(line)
+            # get the length of the new word
+            word_length = len(w)
+            # if adding a new word with *extra one* more white spaces exceeds the maxWidth
+            if new_spaces + word_length + line_length > maxWidth:
+                # iterate up the empty spaces in the line
+                for i in range(maxWidth - line_length):
+                    # push the white spaces to the right if the line has a single word
+                    if len(line)-1 == 0:
+                        line[0] += ' '
+                    # distribute the white spaces in RR if there are multiple word in the line
+                    else:
+                        line[i % (len(line)-1)] += ' '
+                # append the result to the return array
+                res.append(''.join(line))
                 # reset the list of words and total number of chars in each line
-                cur, num_of_letters = [], 0
+                line, line_length = list(), 0
             # append the word for the current line
-            cur.append(w)
+            line.append(w)
             # increase the length of the characters in the current line
-            num_of_letters += len(w)
-
-        # create a left-aligned sentece for the last line
-        for i in range(maxWidth - num_of_letters):
-            if i < len(cur) - 1:
-                cur[0 if len(cur) - 1 == 0 else i % (len(cur) - 1)] += ' '
+            line_length += len(w)
+        # create a left-aligned sentence for the last line
+        for i in range(maxWidth - line_length):
+            # distribute the white spaces in RR once
+            if i < len(line) - 1:
+                line[i % (len(line) - 1)] += ' '
+            # after that push all the other white spaces to the right
             else:
-                cur[len(cur) - 1] += ' '
-        res.append(''.join(cur))
-
+                line[len(line) - 1] += ' '
+        res.append(''.join(line))
         # return the result
         return res
