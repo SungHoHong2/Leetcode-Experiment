7440eefa6d11168e1e12024c84451fdc20f2699f
maverickjin88 <maverickjin88@gmail.com>
2021-01-25 12:17:40 -0700

    explore hard

--- hard/Word_Search_II/README.md
+++ hard/Word_Search_II/README.md
@@ -1,15 +1,39 @@
 ### Word Search II
-**Solution1**
-- [Concepts](images/)
-- [Source code](source/)
-- [Reference #1]()
+**Backtracking with Trie**
+- [Source code](source/Trie.py)
 
-**Solution2**
-- [Concepts](images/)
-- [Source code](source/)
-- [Reference #1]()
+```python
+class Solution:
+    def backtracking(self, row, col, parent):
+        # set an return array 
+        # get the letter from the current cell
+        # get the current node of the Trie
+        # if the matching word is found 
+            # append the word to the returning array 
+            # remove the sentinel to find the word only once 
+        # mark the cell as visited
+        # explore the neighbors
+            # continue if the row or column exceeds the board
+            # continue if the letter does not match the Trie
+            # explore deeper
+        # restore the cell
+        # incrementally remove the chars of the matched word for pruning 
+        pass
 
-**Solution3**
-- [Concepts](images/)
-- [Source code](source/)
-- [Reference #1]()
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # globalize the input 
+        # create a Trie
+        # iterate the words 
+            # set the current layer of the trie
+            # iterate the chars in the word 
+                # create a new layer if the char does not exist 
+                # move on to the next layer
+            # add a sentinel layer once the word is reached  
+        # set a returning array that collects the matched words
+        # iterate the row of the board
+            # iterate the column of the board
+                # if the character matches with the first layer of the Trie
+                    # collect the matched words from  backtracking
+        # return the collected matched words
+        pass
+```
--- hard/Word_Search_II/source/Trie.py
+++ hard/Word_Search_II/source/Trie.py
@@ -1,59 +1,64 @@
 class Solution:
     def backtracking(self, row, col, parent):
+        # set an return array
+        res = list()
         # get the letter from the current cell
         letter = self.board[row][col]
         # get the current node of the Trie
         currNode = parent[letter]
-
-        # check if we find a match of word
+        # if the matching word is found
         if '$' in currNode:
-            self.matchedWords.append(currNode['$'])
+            # append the word to the returning array
+            res.append(currNode['$'])
+            # remove the sentinel to find the word only once
             del currNode['$']
-
         # mark the cell as visited
         self.board[row][col] = '#'
-
         # explore the neighbors
-        for directions in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-            newRow, newCol = row + directions[0], col + directions[1]
+        for r,c in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+            i, j = row + r, col + c
             # continue if the row or column exceeds the board
-            if not (0 <= newRow < len(self.board)) or not (0 <= newCol < len(self.board[0])):
+            if not (0 <= i < len(self.board)) or not (0 <= j < len(self.board[0])):
                 continue
             # continue if the letter does not match the Trie
-            if not self.board[newRow][newCol] in currNode:
+            if not self.board[i][j] in currNode:
                 continue
             # explore deeper
-            self.backtracking(newRow, newCol, currNode)
-
+            res += self.backtracking(i, j, currNode)
         # restore the cell
         self.board[row][col] = letter
-
-        # incrementally remove the matched leaf node in Trie to prune the nodes in Trie
+        # incrementally remove the chars of the matched word for pruning
         if not currNode:
             del parent[letter]
+        return res
 
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # globalize the input
         self.board, self.words = board, words
-        # set a Trie using the input
+        # create a Trie
         trie = {}
+        # iterate the words
         for word in words:
+            # set the current layer of the trie
             node = trie
-            # store the word in the Trie
-            for letter in word:
-                if letter not in node:
-                    node[letter] = dict()
-                node = node[letter]
-            # mark the existence of a word in trie node
+            # iterate the chars in the word
+            for char in word:
+                # create a new layer if the char does not exist
+                if char not in node:
+                    node[char] = dict()
+                # move on to the next layer
+                node = node[char]
+            # add a sentinel layer once the word is reached
             node['$'] = word
-        # set a variable to collect the matched words
-        self.matchedWords = []
+        # set a returning array that collects the matched words
+        res = list()
         # iterate the row of the board
         for row in range(len(board)):
             # iterate the column of the board
             for col in range(len(board[0])):
-                # if the letter matches with the starting letter of the Trie
+                # if the character matches with the first layer of the Trie
                 if board[row][col] in trie:
-                    # invoke the backtracking
-                    self.backtracking(row, col, trie)
+                    # collect the matched words from  backtracking
+                    res += self.backtracking(row, col, trie)
         # return the collected matched words
-        return self.matchedWords
+        return res
